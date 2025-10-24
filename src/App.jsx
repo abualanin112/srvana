dd3ff7a63951a1ca222c66634aa747ad188efde1
abduallah abualanin <abdullahabualanin@gmail.com>
2025-10-24 20:25:35 +0300

    Navbar rtl changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import { Navbar } from "@/components/layout/navbar";
 import Footer from "./components/layout/Footer";
+import { Navbar02 } from "./components/layout/Navbar02";
 
 export default function App() {
-  const [darkMode, setDarkMode] = useState(
-    () => localStorage.getItem("theme") === "dark"
-  );
+  // const [darkMode, setDarkMode] = useState(
+  //   () => localStorage.getItem("theme") === "dark"
+  // );
 
-  useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    }
-  }, [darkMode]);
+  // useEffect(() => {
+  //   if (darkMode) {
+  //     document.documentElement.classList.add("dark");
+  //     localStorage.setItem("theme", "dark");
+  //   } else {
+  //     document.documentElement.classList.remove("dark");
+  //     localStorage.setItem("theme", "light");
+  //   }
+  // }, [darkMode]);
 
   return (
     <BrowserRouter>
       <div className="min-h-screen flex flex-col bg-background text-foreground transition-colors duration-300">
-        <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
-        <main className="flex-grow w-full bg-background px-4 py-8">
+        {/* <Navbar darkMode={darkMode} setDarkMode={setDarkMode} /> */}
+        <Navbar02 />
+        <main dir="rtl" className="flex-grow w-full bg-background px-4 py-8">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<About />} />
